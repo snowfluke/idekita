fb9fe7edcba563459534beb6c15f98850f3670ac
snowfluke <awalariansyah7@gmail.com>
2022-01-02 22:52:25 +0700

    refactor username/slug, icon changed, disabled button

--- components/IdeaContentRight.tsx
+++ components/IdeaContentRight.tsx
@@ -6,9 +6,10 @@ export default function IdeaContentRight({ TopElement, menu }) {
           key={menu.name}
           className="flex-1 rounded-tr-full border-l-4 hover:translate-x-10 b-transition border-dashed border-fuchsia-500 rounded-br-full bg-white rounded-md content-center"
         >
-          <div
+          <button
             onClick={menu.onclick}
-            className="p-5 flex active:bg-fuchsia-500 !cursor-pointer rounded-br-full rounded-tr-full"
+            disabled={menu?.disabled}
+            className="p-5 flex active:bg-fuchsia-500 active:outline-none w-full !cursor-pointer rounded-br-full rounded-tr-full"
           >
             <div className="flex flex-grow text-lg font-bold text-fuchsia-500 active:text-white select-none">
               {menu.name}
@@ -16,7 +17,7 @@ export default function IdeaContentRight({ TopElement, menu }) {
             <div className="flex-none w-8 text-center bg-gray-100 text-lg rounded-full p-[2px] prose justify-self-end select-none">
               {menu.icon}
             </div>
-          </div>
+          </button>
         </div>
       );
     });
