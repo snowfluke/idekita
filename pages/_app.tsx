e4c692e1889087515e0a9f57ec1e9c8fd0f37243
snowfluke <awalariansyah7@gmail.com>
2022-01-02 22:06:03 +0700

    example meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
 import "@styles/globals.css";
 import { UserContext } from "@modules/contexter";
 import { useUserData } from "@modules/hooker";
-import { Navbar, Footer, Toaster } from "@modules/composer";
-import Head from "next/head";
-import { useRouter } from "next/router";
+import { Navbar, Footer, Toaster, Meta } from "@modules/composer";
 
 function MyApp({ Component, pageProps }) {
   const UserData = useUserData();
-  const router = useRouter();
 
   return (
     <UserContext.Provider value={UserData}>
+      <Meta
+        title="iDekita 🌉 Jembatani dengan realisasi"
+        description="Bergabung bersama kami sebagai Idekiawan, mengumpulkan semua ide-ide dan solusi kreatif demi masa depan yang lebih baik lagi"
+      />
       <Navbar />
       <div className="pt-14 mt-10 print:!pt-1 print:!mt-0">
         <main className="w-[80%] mx-auto">
