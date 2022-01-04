6155157431059cb86b50c402cc7a6618f836bc30
Fahrudin <fahroediinj@gmail.com>
2022-01-04 12:43:39 +0700

    Terminologi (daftar istilah dan lencana iDekita)

--- markdowns/terminologies.ts
+++ markdowns/terminologies.ts
@@ -19,6 +19,224 @@ Merupakan istilah untuk menyebut orang yang bergabung dengan iDekita
 👤 - Orang yang berkolaborasi dengan sebuah karya, proyek
 > **Cara mendapatkan:** Lencana hanya dimiliki oleh kontributor Github iDekita
 
+### Adipati 
+👑 - Gelar bangsawan untuk orang yang menjabat sebagai kepala wilayah yang tunduk dalam struktur pemerintahan kerajaan di Nusantara, Adipati di sini sebagai seorang pendukung website iDekita dan tunduk di bawah struktur manajemen iDekita
+> **Cara mendapatkan:** Lencana diberikan kepada pendukung website iDekita
+
+### Ccreator 
+🛠 - Singkatan dari C = Category dan creator adalah pembuat.
+> **Cara mendapatkan:** Lencana akan diperoleh ketika idekiawan mencapai total 20kali(point) dalam pengisian kategori baru
+
+### Astronomer 
+👨‍🚀 - Orang yang memiliki pemikiran seluas alam semesta, yang di sini berarti julukan bagi orang yang memiliki pandangan dalam membuat kategori baru.
+> **Cara mendapatkan:** Lencana akan diperoleh ketika idekiawan mencapai total 40kali(point) dalam pengisian kategori baru
+
+### Pathfinder 
+📌 - Orang yang memikirkan jalan-jalan untuk memudahkan orang-orang dalam mencari jalan (kategori) yang belum terfikirkan oleh orang lain.
+> **Cara mendapatkan:** Lencana akan diperoleh ketika idekiawan mencapai total 60kali(poin) dalam pengisian kategori baru
+
+### Buntel
+🍬 - Sebuah bungkusan, yang dimaksud di sini adalah seseorang yang digambarkan sebuah bungkus, 
+bisa berarti sebuah bungkus ide-ide cemerlang atau hanya angin saja
+> **Cara mendapatkan:** Lencana ini diperoleh pada saat idekiawan membuat akun baru
+
+### Idekreator  
+👨🏻‍💼 - Orang yang membuat/menyampaikan sebuah ide-ide agar bisa dilihat oleh khayalak umum
+> **Cara mendapatkan:** Lencana ini diperoleh ketika idekiawan membuat total 40 postingan ide
+
+### Idepedia 
+👨🏻‍⚖️ - Orang yang memiliki wawasan luas seputar ide-ide baik itu ide dari dirinya maupun ide yang dia dengar dari orang lain
+> **Cara mendapatkan:** Lencana ini diperoleh ketika idekiawan membuat total 80 postingan ide
+
+### Idemitable 
+👨🏻‍🏭- Orang yang menempatkan dirinya sebagai sebuah 'table' atau meja  yang diatasnya terdapat ide-ide cemerlang.
+> **Cara mendapatkan:** Lencana ini diperoleh ketika idekiawan membuat total 120 postingan ide
+
+### Ideamaster 
+🤴🏻- Sesuai dengan yang disematkan di belakang kata ide, yaitu master atau sang ahli, tentunya ahli dalam memikirkan sebuah ide. Entah itu ide yang bermanfaat ataupun tidak.
+> **Cara mendapatkan:** Lencana ini diperoleh ketika idekiawan membuat total 160 postingan ide
+
+### Reportidea  
+🙅‍♂️ - Orang atau perilaku untuk melaporkan sebuah ide orang lain yang dianggapnya tidak bermanfaat/tidak bermutu.
+> **Cara mendapatkan:** Lencana didapatkan ketika idekiawan melaporkan ide sebanyak 50kali
+
+### Reportman 
+👮🏻‍♂️ - Gabungan dari kata “report” & “man” yang artinya adalah seorang pelapor
+> **Cara mendapatkan:** Lencana didapatkan ketika idekiawan melaporkan ide sebanyak 100kali
+
+### Reporteeer 
+💂‍♂️ - Diambil dari kata 'reporter' yang berarti pelapor, terdapat 3 huruf 'e' yang berarti tingkatan ketiga di  urutan tier laporan.
+> **Cara mendapatkan:** Lencana didapatkan ketika idekiawan melaporkan ide sebanyak 150kali
+
+### Sweeper  
+👾 - Bahasa inggris dari kata penyapu, di sini yang bertugas untuk menyapu ide-ide tidak bermutu atau tidak kreatif yang dianggap 'sampah'.
+> **Cara mendapatkan:** Lencana didapatkan ketika idekiawan melaporkan ide sebanyak 200kali
+
+### Judgernaut
+🐻 - Seorang hakim, seorang hakim tentu sudah memiliki banyak pengetahuan seputar apa yang harusnya dia hakimi, di sini berarti orang yang gemar menghakimi ide orang lain.
+> **Cara mendapatkan:** Lencana didapatkan ketika idekiawan melaporkan ide sebanyak 250kali
+
+### Batamaster 
+🤺 - Diambil dari 2 kata 'bata' dan 'master', di mana sebuah bata adalah barang digunakan untuk bahan bangunan yang terbuat dari tanah liat lalu dipanaskan, dan maksud dari sebutan batamaster adalah orang yang memberi dinding pemisah antara pembuat ide-ide yang tidak bermanfaat/kreatif agar tidak terekspos ke khalayak umum.
+> **Cara mendapatkan:** Lencana didapatkan ketika idekiawan melaporkan ide sebanyak 300kali
+
+### Dilirik 
+🤨 - Orang yang mencuri perhatian sehingga orang-orang mulai melirik
+> **Cara mendapatkan:** Lencana ini diterima ketika memperoleh jumlah akumulasi awan 100kali(poin) pada postingannya
+
+### Disenengi  
+😍 - Dalam bahasa Indonesia kata 'disenengi' berarti disukai, yaitu orang-orang yang disukai oleh banyak orang
+> **Cara mendapatkan:** Lencana ini diterima ketika memperoleh jumlah akumulasi awan 200kali(poin) pada postingannya
+
+### Popular 
+✨ - Berbeda dengan kata terkenal, jika terkenal belum tentu disukai, tetapi jika popolar adalah sebutan bagi orang yang terkenal serta disukai oleh orang-orang
+> **Cara mendapatkan:** Lencana ini diterima ketika memperoleh jumlah akumulasi awan 300kali(poin) pada postingannya
+
+### Contributor 
+🧰 - Diakui oleh banyak orang sebagai kontributor, di sini dalam artian orang yang berkontribusi memberikan ide-idenya
+> **Cara mendapatkan:** Lencana ini diterima ketika memperoleh jumlah akumulasi awan 400kali(poin) pada postingannya
+
+### Worldwide 
+🌏 - Dikenal oleh banyak orang tidak hanya satu negara, tapi sudah manca negara
+> **Cara mendapatkan:** Lencana ini diterima ketika memperoleh jumlah akumulasi awan 500kali(poin) pada postingannya
+
+### Superstar 
+🌟 - Mega bintang yang ketenarannya sudah di ambang batas tertinggi
+> **Cara mendapatkan:** Lencana ini diterima ketika memperoleh jumlah akumulasi awan 600kali(poin) pada postingannya
+
+### Pocung 
+👻 - Dalam artian lain kata 'pocung' adalah pocong, yaitu perjalanan terakhir manusia, tetapi semua ketenaran dan kemasyhurannya semasa hidup akan tetap diingat
+> **Cara mendapatkan:** Lencana ini diterima ketika memperoleh jumlah akumulasi awan 700kali(poin) pada postingannya
+
+### Buddy 
+👦 - Seseorang yang memiliki respect terhadap seseorang
+> **Cara mendapatkan:** Lencana didapatkan ketika memberikan awan pada postingan orang lain dengan jumlah 60kali(poin)
+
+### Suwung 
+💁🏻‍♂️ - Orang yang tidak memiliki pekerjaan atau dalam bahasa jawa yaitu 'suwung'
+> **Cara mendapatkan:** Lencana didapatkan ketika memberikan awan pada postingan orang lain dengan jumlah 120kali(poin)
+
+### Penyuka 
+🤙 - Seseorang yang menyukai hasil karya orang lain, dan mulai antusias dalam menunggu postingan-postingan yang lain
+> **Cara mendapatkan:** Lencana didapatkan ketika memberikan awan pada postingan orang lain dengan jumlah 180kali(poin)
+
+### Maniac 
+👋 - Seorang penyuka yang kesukaannya mulai berlebihan atau sangat antusias dan terkesan mulai mengerikan
+> **Cara mendapatkan:** Lencana didapatkan ketika memberikan awan pada postingan orang lain dengan jumlah 240kali(poin)
+
+### Mahajari 
+🤟 - Seseorang yang memiliki jari dewa untuk menyukai setiap postingan yang ada, tanpa memperdulikan itu baik atau buruk
+> **Cara mendapatkan:** Lencana didapatkan ketika memberikan awan pada postingan orang lain dengan jumlah 300kali(poin)
+
+### Sarjana
+🥉 - Sarjana (dari bahasa Sanskerta सर्जन, "penciptaan") adalah gelar akademik yang diberikan kepada lulusan program pendidikan Sarjana (S-1), di iDekita lencana ini diberikan kepada kontributor ide di tingkatan nomer 3.
+> **Cara mendapatkan:** Lencana diperoleh ketika memposting ide dengan kategori Pendidikan dengan jumlah 100 postingan
+
+### Magister
+🥈 - Magister adalah gelar akademik yang diberikan kepada lulusan program pendidikan Magister, kami memberikan lencana ini sebagai apresiasi kepada idekiawan yang turut serta mendukung pendidikan dalam negeri agar menjadi lebih baik lagi.
+> **Cara mendapatkan:** Lencana diperoleh ketika memposting ide dengan kategori Pendidikan dengan jumlah 200 postingan
+
+### Doktor
+🥇 - Doktor adalah gelar akademik yang diberikan kepada lulusan program pendidikan doktor atau strata-3. Biasanya, pemberian gelar doktor membutuhkan pengakuan terhadap kandidat oleh dewan pengajar di universitas tempat dia belajar bahwa ia telah mencapai tingkat yang setara dengan para anggota dewan itu. Di iDekita, orang yang memiliki lencana ini adalah orang yang diakui langsung oleh kami.
+> **Cara mendapatkan:** Lencana diperoleh ketika memposting ide dengan kategori Pendidikan dengan jumlah 300 postingan
+
+### Teknogidea 
+👩🏻‍💻 - Orang yang memiliki ide-ide seputar teknologi
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 20 ide dengan tema Teknologi 
+
+### Teknoddict 
+👨‍💻 - Seseorang yang berambisi dalam teknologi
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 40 ide dengan tema Teknologi
+
+### Teknofreak 
+🤯 - Sebutan untuk orang yang sangat menyukai teknologi dan berambisi untuk mewujudkan segala cita-citanya dalam mencipatkan teknologi baru
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 60 ide dengan tema Teknologi
+
+### Teknoboss 
+🧞 - Gabungan dari kata 'tekno' dan 'boss', tekno yaitu teknologi sedangkan boss merujuk ke dalam sebutan dari raja para monster, 
+di sini berarti sebutan bagi orang yang memiliki keahlian dalam bidang teknologi yang kemampuannya setara dengan boss para monster
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 80 ide dengan tema Teknologi
+
+### Pertidea 
+👩‍🌾 - Orang yang peduli dalam bidang pertanian
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 20 ide dengan tema Pertanian
+
+### Pertaddict 
+💪 - Seseorang yang berambisi dalam kemajuan di bidang pertanian
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 40 ide dengan tema Pertanian
+
+### Pertafreak 
+🦶 - Sebutan bagi orang yang memiliki cita-cita untuk membuat bidang pertanian menjadi sangat maju dan memiliki terobosan-terobosan yang tidak terpikirkan oleh orang lain sebelumnya
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 60 ide dengan tema Pertanian
+
+### Pertaboss
+👴 - Orang yang sudah sangat ahli di bidangnya dan ingin terus memajukkan bidang pertanian demi kemajuan umat manusia.
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 80 ide dengan tema Pertanian
+
+### Creatividea 
+👨‍🔧 - Orang yang berfikir kreatif
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 20 ide dengan tema Kreatifitas
+
+### Creatividdict 
+🎭 - Orang yang berpikiran kreatif namun lebih antusias.
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 40 ide dengan tema Kreatifitas
+
+### Creatifreak 
+🕵️ - Sebutan bagi seseorang yang berpikir kreatif seperti orang aneh, karna semua hal tercipta dari kreatifitas berfikir dan sering dianggap aneh oleh kebanyakan orang
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 60 ide dengan tema Kreatifitas
+
+### Creativiboss 
+👨🏻‍🎨 - Orang yang berpikiran kreatif melebihi manusia biasa, dan setiap buah pemikirannya akan menciptakan sesuatu hal baru yang tidak pernah terfikirkan oleh orang biasa
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 80 ide dengan tema Kreatifitas
+
+### Accnoob 
+🤦 - Seorang yang menekuni bidang akuntansi
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 20 ide dengan tema Akuntansi
+
+### Accruiser  
+👲 - Gabungan dari kata 'acc' = accountancy dan 'cruiser' = penjelajah, adalah orang yang mulai menjelajah dan bereksplorasi dalam bidang akuntasi
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 40 ide dengan tema Akuntansi
+
+### Accmaster 
+👨🏻‍🏫 - Seseorang yang memiliki kemampuan di atas rata-rata dalam bidang akuntansi
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 60 ide dengan tema Akuntansi
+
+### Accqueentance  
+👸 - Gabungan dari kata 'acc' = akun dan 'queen' = ratu dan ditambah -tance di belakang, yang berarti orang yang sangat ahli di bidang akuntansi dan kemampuan serta cara manajemen keuangan sangat tinggi seperti seorang ratu.
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 80 ide dengan tema Akuntansi
+
+### Appkiddie 
+🙆‍♂️ - Gabungan dari kata 'app' singkatan kata aplikasi dalam bahasa inggris serta 'kiddie' yaitu sebutan bagi seorang programmer pemula.
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 20 ide dengan tema perancangan/pembuatan Aplikasi
+
+### Appmaker 
+🧛 - Seseorang yang sudah biasa membuat aplikasi.
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 40 ide dengan tema perancangan/pembuatan Aplikasi
+
+### Appholic 
+🦸 - Sebutan bagi orang yang sangat menyukai aplikasi
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 60 ide dengan tema perancangan/pembuatan Aplikasi
+
+### Appsodist 
+🤹🏻 - Gabungan dari kata 'app' yang berarti aplikasi dan 'sodist' yang diambil dari kata 'rhapsodist' yang digunakan sebagai sebutan orang ahli sastra, di sini appsodist berarti orang yang ahli dalam bahasa pemrograman.
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 80 ide dengan tema perancangan/pembuatan Aplikasi
+
+### Tradetrain 
+🧝 - Gabungan dari kata “trade” yang berarti transaksi dan “train” diambil dari kata “trainee” yang berarti pemula, adalah orang yang masih belajar di bidang trading.
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 20 ide dengan tema Trading
+
+### Tradewhaller 
+👳‍♀️ - Orang yang paham dan mengetahui struktur dan algoritma dalam dunia trading
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 40 ide dengan tema Trading
+
+### Tradeasy 
+🤵 - Gabungan dari kata 'trade' dan 'easy' yang berarti adalah orang yang sudah ahli di bidang trading
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 60 ide dengan tema Trading
+
+### Tradebweezard 
+🧙‍♂️ - Seseorang yang memiliki kemampuan seorang 'wizard', di mana seorang wizard atau penyihir memiliki otak dan kemampuan berfikir yang sangat cerdas, di sini berarti orang yang berkemampuan dalam mengelola trading di tingkatan seorang ahli.
+> **Cara mendapatkan:** Lencana diberikan kepada orang yang membagikan total 80 ide dengan tema Trading
+
 `;
 
 export { terminologies };
